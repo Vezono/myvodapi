20837a31e6ff48d29f4cc2e229da7ff7ca328d78
vezono <vezonolab@gmail.com>
2020-11-14 18:02:45 +0200

    prettify requesting. there will be exceptions soon!

--- myvodapi/core.py
+++ myvodapi/core.py
@@ -13,64 +13,59 @@ class Session:
     def params(self):
         return {"version": self.version, "language": self.language, "source": self.source, "token": self.token}
 
+    def request(self, method="", params=None, json=None):
+        if not params:
+            params = {}
+        if not json:
+            json = {"params": self.params, "requests": {method: params}}
+        return request('POST', url=self.api_url, json=json).json()
+
     def login(self, number: str):
         tempToken = self.callSmsWithTempToken(number)
         self.token = self.enterCodeAndGetAccessToken(number, input('Enter your code: '), tempToken)
         print(f'Congrats! You are logged in! Your access token: \n{self.token}')
 
-    def getLinks(self):
-        json = {"params": self.params, "requests": {"getLinks": {}}}
-        return request('POST', url=self.api_url, json=json).json()
-
-    def getSettings(self):
-        json = {"params": self.params, "requests": {"settings": {}}}
-        return request('POST', url=self.api_url, json=json).json()
-
-    def callSmsCode(self, number: str):
-        json = {"params": self.params, "requests": {"loginV2": {"id": number}}}
-        return request('POST', url=self.api_url, json=json).json()
-
     def callSmsWithTempToken(self, number: str):
-        return self.callSmsCode(number)['tempToken']
+        return self.callSmsCode(number)["loginV2"]["values"]['tempToken']
 
     def enterSmsCode(self, number: str, code: str, tempToken: str):
         json = {"params": self.params, "requests": {
                 "getToken": {"id": number, "tpass": code, "puk": "", "rememberMe": None,
                 "tempToken": tempToken, "action": "0", "parentToken": ""}}}
-        return request('POST', url=self.api_url, json=json).json()
+        return self.request(json=json)
 
     def enterCodeAndGetAccessToken(self, number: str, code: str, tempToken: str):
         return self.enterSmsCode(number, code, tempToken)['getToken']['values']['token']
 
+    def getLinks(self):
+        return self.request('getLinks')
+
+    def getSettings(self):
+        return self.request('settings')
+
+    def callSmsCode(self, number: str):
+        return self.request('loginV2', {'id': number})
+
     def getRelPhone(self):
-        json = {"params": self.params, "requests": {"getRelPhone": {}}}
-        print(json)
-        return request('POST', url=self.api_url, json=json).json()
+        return self.request("getRelPhone")
 
     def getActiveOffer(self):
-        json = {"params": self.params, "requests": {"getActiveOffer": {}}}
-        return request('POST', url=self.api_url, json=json).json()
+        return self.request("getActiveOffer")
 
     def getBalance(self):
-        json = {"params": self.params, "requests": {"balance": {}}}
-        return request('POST', url=self.api_url, json=json).json()
+        return self.request("balance")
 
     def getInternetCounters(self):
-        json = {"params": self.params, "requests": {"countersMainDPIv3": {}}}
-        return request('POST', url=self.api_url, json=json).json()
+        return self.request("countersMainDPIv3")
 
     def getCallsAndSmsCounters(self):
-        json = {"params": self.params, "requests": {"countersMainV2": {}}}
-        return request('POST', url=self.api_url, json=json).json()
+        return self.request("countersMainV2")
 
     def getCurrentPlan(self):
-        json = {"params": self.params, "requests": {"currentPlan": {}}}
-        return request('POST', url=self.api_url, json=json).json()
+        return self.request("currentPlan")
 
     def getBonus(self):
-        json = {"params": self.params, "requests": {"getBonus": {}}}
-        return request('POST', url=self.api_url, json=json).json()
+        return self.request("getBonus")
 
     def getCreditBalance(self):
-        json = {"params": self.params, "requests": {"getAMACreditBalance": {}}}
-        return request('POST', url=self.api_url, json=json).json()
+        return self.request("getAMACreditBalance")
